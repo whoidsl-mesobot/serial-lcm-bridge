75a87f72136f088bbc76ddedaf12d83bc293dbab
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-06-30 22:12:08 -0700

    add draft bridge implementation in python

---
+++ python/line-bridge.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import time
+import select
+import serial
+import lcm
+import raw_bytes_t
+
+
+# TODO: Implement this as a bridge class with sio and lio attribute
+# TODO: extend to include multiple sio<=>lio lanes
+
+def publish_serial_line(raw, lio, channel):
+    msg = raw_bytes_t()
+    msg.timestamp = int(time.time() * 1e6)
+    msg.size = len(raw)
+    msg.raw = list(raw)
+    lio.publish(channel, msg.encode())
+
+def raw_handler(channel, data):
+    msg = raw_bytes_t.decode(data)
+    sio.write(''.join(c for c in msg.raw))
+
+sio = serial.Serial(port='/dev/ttyS0', baudrate=9600, timeout=0)
+sio.nonblocking()
+lio = lcm.LCM()
+
+inputs = [sio, lio]
+# inputs = [sio.fileno(), lio.fileno()]
+outputs = inputs
+errors = [] # TODO: catch and handle errors
+
+serial_buffer = '' # TODO: try implementing as a deque
+serial_lines = []
+
+try:
+    timeout = 0.1 # in seconds TODO: test removing
+    while True:
+        readable, writable, exceptional = select.select(inputs, outputs, errors, timeout)
+        if sio in readable: 
+            serial_buffer += sio.read(sio.inWaiting())
+        if '/n' in serial_buffer:
+            serial_lines.extend(serial_buffer.split('\n'))
+            serial_buffer = serial_lines.pop(-1)
+        while len(serial_lines) > 0 and lio in writable: 
+            publish_serial_line(serial_lines.pop(1) + '\n', lio, channel)
+        if lio in readable and sio in writable: 
+            lio.handle()
+except KeyboardInterrupt:
+    print('Terminated by user.')
