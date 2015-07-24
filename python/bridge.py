9f2ec7a9110f3f34a0b86abffa3005012d1a9c50
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2015-07-23 19:09:00 -0700

    working BinaryLane example by replaying RTI ADCP ensemble file over serial port with null modem

--- python/bridge.py
+++ python/bridge.py
@@ -2,8 +2,11 @@
 
 import time
 import select
-import serial
+import struct
+
 import lcm
+import serial
+import crcmod
 
 from lcmtypes import raw_bytes_t, line_t
 
@@ -71,15 +74,15 @@ class Lane:
         self.sio.baudrate = baudrate
         self.sio.open()
         self.sio.nonblocking()
-        
+
         self.sio.subscribe('.'.join((channel, 'in')), self.serial_handler)
         self.lio.subscribe('.'.join((channel, 'out')), self.lcm_handler)
-        
+
         ios = (self.sio, self.lio)
         try:
             while True:
                 readable, writable, exceptional = select.select(ios, ios, ios)
-                if self.verbosity > 2: # TODO: use logging module instead
+                if self.verbosity > 3: # TODO: use logging module instead
                     print('readable:{0}'.format(readable))
                     print('writable:{0}'.format(writable))
                     print('exceptional:{0}'.format(exceptional))
@@ -100,7 +103,8 @@ class Lane:
 
 class TextLane(Lane):
 
-    def __init__(self, sio=SerialWithHandler(), lio=lcm.LCM(), verbosity=0, delimiter=b'\n'):
+    def __init__(self, sio=SerialWithHandler(), lio=lcm.LCM(), verbosity=0,
+            delimiter=b'\n'):
         if type(delimiter) is str: delimiter = delimiter.encode()
         self.msg = line_t()
         self.delimiter = delimiter
@@ -126,8 +130,16 @@ class TextLane(Lane):
 
 class BinaryLane(Lane):
 
-    def __init__(self, sio=SerialWithHandler(), lio=lcm.LCM(), verbosity=0):
+    def __init__(self, sio=SerialWithHandler(), lio=lcm.LCM(), verbosity=0,
+            preamble=b'\x80'*16,
+            header_struct=struct.Struct('i'*8), header_payload_size_index = 6,
+            crc_struct = struct.Struct('<I'), crc = None):
         self.msg = raw_bytes_t()
+        self.preamble = preamble
+        self.header_struct = header_struct
+        self.header_payload_size_index = header_payload_size_index
+        self.crc_struct = crc_struct
+        self.crc = crc
         super().__init__(sio, lio, verbosity)
 
     def lcm_handler(self, channel, data):
@@ -137,15 +149,56 @@ class BinaryLane(Lane):
 
     def serial_handler(self, channel, data):
         self.q.extend(data)
-        msg.timestamp = self.sio.readtime
+        self.q = self.q[self.q.find(self.preamble):] # move to next preamble
+        ph_size = len(self.preamble) + self.header_struct.size
+        if len(self.q) > ph_size:
+            if self.verbosity > 1: print('checking {0}-byte queue'.format(len(self.q)))
+            if self.verbosity > 2: print('preamble and header: {0}'.format(self.q[:ph_size]))
+            hdr = self.header_struct.unpack_from(self.q, len(self.preamble))
+            if self.verbosity > 2: print('read header: {0}'.format(hdr))
+            payload_size = hdr[self.header_payload_size_index]
+            self.msg.size = ph_size + payload_size + self.crc_struct.size
+#            while len(self.q) < self.msg.size: # and self.sio.inWaiting():
+#                self.q.extend(self.sio.read(self.sio.inWaiting()))
+#                time.sleep(0.1)
+            if len(self.q) >= self.msg.size:
+                if self.verbosity > 1: print('reading {0}-byte payload'.format(payload_size))
+                checksum_calculated = checksum_read = 0
+                if self.crc is not None:
+                    checksum_read = self.crc_struct.unpack_from(self.q, ph_size + payload_size)[0]
+                    checksum_calculated = self.crc(self.q[ph_size : ph_size + payload_size])
+                if checksum_calculated == checksum_read:
+                    self.msg.timestamp = self.sio.readtime
+                    self.msg.raw = self.q[:self.msg.size]
+                    self.lio.publish(channel, self.msg.encode())
+                    if self.verbosity > 0: print('sent {m.size} bytes'.format(m=self.msg))
+                else:
+                    print('checksum mismatch: calculated {}, read {}'.format(checksum_calculated, checksum_read))
+                    # TODO: Send a notification of mismatch via LCM.
+                self.q = self.q[self.msg.size:]
+                if self.verbosity > 0: print('keeping {0}-byte queue'.format(len(self.q)))
+            else:
+                if self.verbosity > 1: print('waiting for {0} bytes'.format(self.msg.size))
         if len(self.q) > self.qmax:
             print('queue too long, clearing')
             self.q.clear()
+            # TODO: Send a notification of overflow via LCM.
+
+
+#def main(port, channel=None, baudrate=9600, verbosity=0):
+#    if channel is None: channel = port.split('/')[-1]
+#    lane = TextLane(verbosity=verbosity)
+#    lane.open(port, channel, baudrate)
 
 
 def main(port, channel=None, baudrate=9600, verbosity=0):
     if channel is None: channel = port.split('/')[-1]
-    lane = TextLane(verbosity=verbosity)
+    lane = BinaryLane(verbosity=verbosity,
+            preamble=b'\x80'*16,
+            header_struct=struct.Struct('i'*4),
+            header_payload_size_index = 2,
+            crc_struct = struct.Struct('<I'),
+            crc=crcmod.predefined.mkPredefinedCrcFun('xmodem'))
     lane.open(port, channel, baudrate)
 
 
